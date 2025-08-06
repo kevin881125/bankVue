d6a1264bb1f430b38acbf6dda962a7728314a093
kevin <k0978237933@gmail.com>
2025-08-06 20:49:23 +0800

    no message

--- bankProject/src/views/back/member/member.vue
+++ bankProject/src/views/back/member/member.vue
@@ -51,27 +51,7 @@
     <table>
       <thead>
         <tr>
-<<<<<<< HEAD
-          <th style="width: 5%;">編號</th>
-          <th style="width: 12%;">名子</th>
-          <th style="width: 8%;">身分證</th>
-          <th style="width: 5%;">性別</th>
-          <th style="width: 12%;">出生</th>
-          <th style="width: 12%;">電話</th>
-          <th style="width: 20%;">信箱</th>
-          <th style="width: 12%;">創辦日期</th>
-          <th >狀態</th>
-          <th >查看</th>
-          <th >修改</th>
-        </tr>
-      </thead>
-      <memberRow
-        v-for="m in members"
-        :member="m"
-        @click="openModal"
-        @edit="edit"
-      ></memberRow>
-=======
+
           <th>編號</th>
           <th>名子</th>
           <th>身分證</th>
@@ -85,7 +65,6 @@
         </tr>
       </thead>
       <memberRow v-for="m in members" :member="m"></memberRow>
->>>>>>> loan
     </table>
   </div>
   <!-- 查看詳細資料-->
@@ -460,13 +439,10 @@ table {
   width: 100%;
   background-color: rgb(255, 255, 255);
   border-collapse: collapse;
-<<<<<<< HEAD
-  box-shadow: 0 0 2px 2px rgba(0, 0, 0, 0.1);
-=======
+
   box-shadow: 0 0 10px 10px gray;
   padding-left: 10px;
   padding-right: 10px;
->>>>>>> loan
 }
 thead {
   width: 100%;
@@ -487,11 +463,8 @@ th {
 }
 .search {
   width: 100%;
-<<<<<<< HEAD
   height: calc(8% - 10px);
-=======
-  height: calc(15% - 10px);
->>>>>>> loan
+
   background-color: #ffffff;
   margin-bottom: 10px;
   box-shadow: 0 4px 12px rgba(83, 83, 83, 0.1);
