c8d8251635783c4cd6c8a7329ebb1793b50e1e2a
LilyLilyChiu <Liling01327@gmail.com>
2025-08-08 17:13:22 +0800

    8/8 1712新增、修改按鈕 功能

--- bankProject/src/views/back/account/account.vue
+++ bankProject/src/views/back/account/account.vue
@@ -1,10 +1,9 @@
 <template>
   <v-app>
-    <v-main>
-      <v-container>
+    <v-main >
+      <v-container style="max-width: 1500px; margin: auto">
         <!-- 查詢區 -->
-        <v-row>
-          <v-col cols="12" sm="1">搜尋:</v-col>
+        <v-row class="search-area">
           <v-col cols="12" sm="2">
             <v-text-field v-model="filters.mId" label="客戶編號"></v-text-field>
           </v-col>
@@ -29,20 +28,45 @@
               label="姓名搜尋"
             ></v-text-field>
           </v-col>
-          <v-col cols="12" sm="1" class="d-flex align-center">
-            <v-btn
-              @click="fetchAccounts"
-              variant="plain"
-              size="large"
-              class="px-6"
-              prepend-icon="mdi-magnify"
-              >查詢</v-btn
-            >
+          
+          <!-- 查詢按鈕 -->
+          <v-col cols="12" sm="2" class="d-flex justify-space-between align-center">
+              <v-btn
+                @click="fetchAccounts"
+                variant="plain"
+                size="large"
+                class="px-6"
+                prepend-icon="mdi-magnify"
+              >
+                查詢
+              </v-btn>
+
+              <!-- 新增帳戶 -->
+              <v-tooltip location="top">
+                <template #activator="{ props }">
+                  <v-btn v-bind="props" icon size="large" @click="openAddDialog">
+                    <v-icon>mdi-account-edit-outline</v-icon>
+                  </v-btn>
+                </template>
+                <span>新增帳戶</span>
+              </v-tooltip>
+              <!-- 人工凍結檢查 -->
+              <v-tooltip location="top">
+                <template #activator="{ props }">
+                  <v-btn v-bind="props" icon size="large" @click="openCheckDialog">
+                    <v-icon>mdi-account-sync-outline</v-icon>
+                  </v-btn>
+                </template>
+                <span>人工凍結檢查</span>
+              </v-tooltip>
+
+
           </v-col>
         </v-row>
 
         <!-- 資料表 -->
-        <v-data-table :headers="headers" :items="filteredAccounts">
+        
+        <v-data-table :headers="headers" :items="filteredAccounts" :items-per-page="5" :items-per-page-options="[5, 10, 20]" class="my-data-table" >
           <template v-slot:item.balance="{ item }">
             {{ item.currency }} {{ item.balance.toLocaleString() }}
           </template>
@@ -50,7 +74,7 @@
             <div class="d-flex" style="gap: 10px">
               <v-tooltip location="top">
                 <template #activator="{ props }">
-                  <v-btn v-bind="props" icon size="small">
+                  <v-btn v-bind="props" icon size="large" @click="tradeDialog = true">
                     <v-icon> mdi-hand-coin-outline</v-icon>
                   </v-btn>
                 </template>
@@ -58,7 +82,7 @@
               </v-tooltip>
               <v-tooltip location="top">
                 <template #activator="{ props }">
-                  <v-btn v-bind="props" icon size="small">
+                  <v-btn v-bind="props" icon size="large" @click="detailDialog = true">
                     <v-icon> mdi-receipt-text-outline</v-icon>
                   </v-btn>
                 </template>
@@ -66,7 +90,7 @@
               </v-tooltip>
               <v-tooltip location="top">
                 <template #activator="{ props }">
-                  <v-btn v-bind="props" icon size="small">
+                  <v-btn v-bind="props" icon size="large" @click="openUpdateDialog(item)">
                     <v-icon>mdi-account-edit-outline</v-icon>
                   </v-btn>
                 </template>
@@ -76,6 +100,79 @@
             </div>
           </template>
         </v-data-table>
+
+        <!-- 新增帳戶 addDialog -->
+        <v-dialog v-model="addDialog" width="300">
+          <v-card>
+            <v-card-title>新增帳戶</v-card-title>
+            <v-card-text>
+              <v-text-field v-model="newAccount.mId" clearable label="會員編號"></v-text-field>
+              <v-text-field v-model="newAccount.accountName" clearable label="帳戶名稱"></v-text-field>
+              <v-select v-model="newAccount.currency" label="幣別" :items="['台幣', '外幣']"></v-select>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text @click="submitNewAccount">新增</v-btn>
+              <v-btn text @click="addDialog = false">取消</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+        <!-- 交易 tradeDialog -->
+        <v-dialog v-model="tradeDialog" width="500">
+          <v-card>
+            <v-card-title>交易</v-card-title>
+            <v-card-text>這裡放交易功能</v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn text @click="tradeDialog = false">關閉</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+        <!-- 明細 detailDialog -->
+        <v-dialog v-model="detailDialog" width="500">
+          <v-card>
+            <v-card-title>交易明細</v-card-title>
+            <v-card-text>這裡放交易明細</v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn text @click="detailDialog = false">關閉</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>        
+        <!-- 修改 updateDialog -->
+        <v-dialog v-model="updateDialog" width="400">
+          <v-card>
+            <v-card-title>修改帳戶狀態</v-card-title>
+            <v-card-text>
+              <v-row class="mb-3">
+                <v-col cols="12" class="text-h6">
+                  客戶名稱：{{ selectedAccount.member?.mName }}
+                </v-col>
+                <v-col cols="12" class="text-h6">
+                  帳戶名稱：{{ selectedAccount.accountName }}
+                </v-col>
+                <v-col cols="12" class="text-h6">
+                  帳戶號碼：{{ selectedAccount.accountId }}
+                </v-col>
+              </v-row>   
+              <v-row dense>
+                  <v-col cols="12" >           
+                    <v-select v-model="updateForm.status" label="狀態" :items="['啟用','凍結','限制']"></v-select>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-textarea v-model="updateForm.memo" label="備註" rows="2" auto-grow />
+                  </v-col> 
+                </v-row>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn text @click="submitUpdateStatus">確定</v-btn>
+              <v-btn text @click="updateDialog = false">取消</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+
+
       </v-container>
     </v-main>
   </v-app>
@@ -84,11 +181,32 @@
 <script setup>
 import { request } from "@/utils/BackAxiosUtil";
 import { ref, onMounted, watch } from "vue";
+import { useWorkerStore } from "@/stores/Worker";
 
-const search = ref("");
+const addDialog = ref(false);
+const tradeDialog = ref(false);
+const updateDialog = ref(false);
+const detailDialog = ref(false);
 const filteredAccounts = ref([]);
+const selectedAccount = ref(null);
+const workerStore = useWorkerStore();
+const workerId = workerStore.wId; // 操作行員ID
 
+// 搜尋欄位
 const fetchAccounts = async () => {
+ 
+  /**
+   * 確認有至少一個欄位填寫**** 不然會報錯 ！
+   * Object.values() 會把物件的所有值取出，變成陣列
+   * .every() 是陣列方法，會對陣列每一個元素跑判斷
+   * val 是陣列裡的每一個欄位值
+   */
+  const isAllEmpty = Object.values(filters.value).every(val => !val || val.trim() === "");
+
+  if ( isAllEmpty ) {
+    alert("請輸入查詢條件")
+  }
+
   const requestBody = {
     mId: filters.value.mId || null,
     accountId: filters.value.accountId || null,
@@ -117,6 +235,7 @@ const filters = ref({
   mName: "",
 });
 
+// table 抬頭 
 const headers = [
   { title: "客戶編號", key: "mId" },
   { title: "客戶姓名", key: "member.mName" },
@@ -124,6 +243,129 @@ const headers = [
   { title: "帳戶號碼", key: "accountId" },
   { title: "帳戶名稱", key: "accountName" },
   { title: "餘額", key: "balance" },
+  { title: "狀態", key: "status"},
   { title: "操作", key: "actions", sortable: false },
 ];
+
+// 新增帳戶 
+const openAddDialog = () => {
+  newAccount.value = {mId: "", accountName: "", currency: ""},
+  addDialog.value = true;
+}
+
+const newAccount = ref({
+  mId: "",
+  accountName: "",
+  currency: ""
+});
+
+const submitNewAccount = async () => {
+  if(
+    !newAccount.value.mId || !newAccount.value.accountName || !newAccount.value.currency
+  ){
+    alert("請填寫所有欄位")
+    return;
+  }
+
+  // 驗證會員編號是否存在
+  try {
+    const mIdresult = await request({
+      url:"/member/"+newAccount.value.mId,
+      method:"GET",
+    });
+  
+    if(!mIdresult){
+      alert("會員編號不存在")
+      return;
+    }
+  
+    if( newAccount.value.currency === '台幣' ){
+      newAccount.value.currency = 'NT'
+    }
+    
+    const result = await request({
+      url:"/account/insert",
+      method:"POST",
+      data:{
+        mId: newAccount.value.mId,
+        accountName: newAccount.value.accountName,
+        currency: newAccount.value.currency,
+      },
+      headers: { "Content-Type": "application/json" },
+    });
+  
+    alert("新增帳戶成功");
+    addDialog.value = false; //關閉dialog
+    newAccount.value = {mId:"", accountName:"", currency:""}; // 清空欄位
+    
+  } catch (error) {
+    alert("新增失敗，請稍後再試")
+    console.error(error);
+  }
+}
+
+// 修改帳戶狀態
+const updateForm = ref({
+  status:"",
+  memo:" ",
+  operatorId:"",
+  accountId:"",
+});
+
+const openUpdateDialog = (item) =>{
+  selectedAccount.value = item;
+  updateForm.value.status = item.status || '';
+  updateForm.value.operatorId = workerId;
+  updateForm.value.accountId = item.accountId;
+  updateDialog.value = true;
+  updateForm.value.memo = "";
+};
+
+const submitUpdateStatus = async () => {
+  if( updateForm.value.status === selectedAccount.value.status ){
+    alert("請修改狀態");
+    return;
+  }
+
+  // memo是null的話 => false 是空白的話.trim() => ""空白刪掉 => 沒有東西所以是false 
+  if( !updateForm.value.memo?.trim() ){
+      alert("請填寫修改狀態原因");
+      return;
+    }
+    console.log(updateForm);
+
+    const updateRS = await request({
+      url:"/account/update",
+      method:"PUT",
+      data: updateForm.value,
+       headers: { "Content-Type": "application/json" },
+    });
+
+    alert("修改狀態成功");
+    updateDialog.value = false;
+
+  }
 </script>
+
+<style scoped>
+  .my-data-table >>> .v-data-table-header th {
+  font-size: 20px; /* header 字體 */
+  font-weight: bold;
+}
+
+.my-data-table >>> .v-data-table__td {
+  font-size: 17px; /* 內文字體 */
+  font-weight: normal; /* 不要粗體 */
+  vertical-align: middle; /* 垂直置中 */
+  padding-top: 12px;
+  padding-bottom: 12px; /* 上下間距 */
+  
+}
+.my-data-table{
+  margin-top: 20px;
+}
+.search-area{
+  margin-top: 20px;
+}
+
+</style>
