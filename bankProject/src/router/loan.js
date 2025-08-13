2ade915b1a678434d2313dda460c5f6b78cf577f
mike <dy9638522@gmail.com>
2025-08-13 18:01:02 +0800

    no message

--- bankProject/src/router/loan.js
+++ bankProject/src/router/loan.js
@@ -1,6 +1,13 @@
 export default [
+  // 後台
   {
     path: "loan",
     component: () => import("@/views/back/loan/loan.vue"),
+  },
+
+  // 前台
+  {
+    path: "loanHome",
+    component: () => import("@/views/front/loan/loan.vue"),
   },
 ];
